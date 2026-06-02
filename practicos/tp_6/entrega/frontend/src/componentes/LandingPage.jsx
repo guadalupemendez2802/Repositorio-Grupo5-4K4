14a3b7e5e25dd18bcceac9e58c76d1736b33b0e3
tinchoagc <tinchoagc54@gmail.com>
2026-06-02 16:00:19 -0300

    feat: reemplazo de imagen hero por logo del parque en LandingPage

--- practicos/tp_6/entrega/frontend/src/componentes/LandingPage.jsx
+++ practicos/tp_6/entrega/frontend/src/componentes/LandingPage.jsx
@@ -1,11 +1,11 @@
-import heroImg from '../assets/hero.png'
+import logoImg from '../assets/logo_parque.png'
 import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
   const navigate = useNavigate()
   return (
     <section id="center"> 
-      <img src={heroImg} alt="Parque tematico" width={320} />
+      <img src={logoImg} alt="Logo del Parque" width={320} />
       <h1>Comprá tu entrada</h1>
       <p>Reservá tu visita al parque de forma rápida y segura.</p>
       <button type="button" className="btn-primary" onClick={() => {navigate("/buy")}}>

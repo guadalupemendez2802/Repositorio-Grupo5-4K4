5551f7ad0d1ac23b85a0c65d46a6157319de0dab
Ivo <91078@sistemas.frc.utn.edu.ar>
2026-06-02 16:24:13 -0300

    fix: actualizar URL del frontend en ticketsForm.jsx

--- practicos/tp_6/entrega/frontend/src/componentes/ticketsForm.jsx
+++ practicos/tp_6/entrega/frontend/src/componentes/ticketsForm.jsx
@@ -11,7 +11,7 @@ import {
 import { useNavigate } from "react-router-dom";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000'
-const FRONTEND_BASE_URL = 'http://192.168.1.125:5173'
+const FRONTEND_BASE_URL = 'http://192.168.1.125:5174'
 
 function ModalCompraExitosa({ compra, emailUsuario, entradas, onCerrar }) {
   const fechaFormateada = compra.fecha_visita
